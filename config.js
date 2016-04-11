b1b0867b7c32c6d3120f6ad778f3214248c45026
unHealthy <chenzhutian1992@gmail.com>
2016-04-12 00:37:25 +0800

    init

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   build: {
     index: path.resolve(__dirname, 'dist/index.html'),
     assetsRoot: path.resolve(__dirname, 'dist'),
-    assetsSubDirectory: '/src/static',
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true
   },
