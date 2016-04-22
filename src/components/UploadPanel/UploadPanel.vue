0ac333fe9c791467e4af184a65d702059c0f19c8
unHealthy <chenzhutian1992@gmail.com>
2016-04-22 17:34:54 +0800

    init upload panel component

---
+++ src/components/UploadPanel/UploadPanel.vue
@@ -0,0 +1,27 @@
+<template src="./UploadPanel.html"></template>
+<script src="./UploadPanel.js"></script>
+<style scoped>
+    .upload-panel {
+        position: fixed;
+        bottom: 48px;
+        right: 48px;
+    }
+    
+    .upload-dialog {}
+    
+    .upload-header {
+        width: 100%;
+        min-height: 48px;
+    }
+    
+    .upload-form {
+        max-width: 80%;
+        margin: 0 auto;
+        padding-bottom: 48px;
+        padding-top: 28px;
+    }
+    
+    .upload-form .mdl-textfield {
+        width: 100%;
+    }
+</style>
