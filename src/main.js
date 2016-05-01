757077dfdcbc4b8cba4d0eed424319646fd353a0
unHealthy <chenzhutian1992@gmail.com>
2016-05-01 15:07:32 +0800

    no more filters

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue';
 import App from './components/App/App.vue';
 
-// filters
-import tagCheckBoxIcon from './filters/tagCheckboxIcon';
-Vue.filter('tagCheckBoxIcon', tagCheckBoxIcon);
-
 /* eslint-disable no-new */
 new Vue({
     el: 'body',
