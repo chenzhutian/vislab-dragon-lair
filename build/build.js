bb3d3764c38dfc51508572dd84633cd329d4df00
unHealthy <chenzhutian1992@gmail.com>
2016-05-01 15:00:45 +0800

    fix some eslint issue

--- build/build.js
+++ build/build.js
@@ -26,11 +26,11 @@ shelljs.cp('-R', 'static/', assetsPath);
 webpack(webpackConfig, (err, stats) => {
     spinner.stop();
     if (err) throw err;
-    process.stdout.write(stats.toString({
+    process.stdout.write(`${stats.toString({
         colors: true,
         modules: false,
         children: false,
         chunks: false,
         chunkModules: false,
-    }) + '\n');
+    })}\n`);
 });
